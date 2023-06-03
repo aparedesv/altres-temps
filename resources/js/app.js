e13a8aa3b02ded6bb1287c859febc5fec2e5b187
aparedesv <62504000+aparedesv@users.noreply.github.com>
2023-06-03 09:36:02 +0200

    afegir marks al mapa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,39 @@
 import './bootstrap';
 
-import { Map as OLMap, View as OLView } from 'ol';
+/* import { Map as OLMap, View as OLView } from 'ol';
 import { Tile as OLTileLayer } from 'ol/layer';
 import { OSM as OLOsm } from 'ol/source';
-import * as OLProj from 'ol/proj';
-// import OLMap from 'ol/Map';
-// import OLView from 'ol/View';
-// import OLTileLayer from 'ol/layer/Tile';
-// import OLOsm from 'ol/source/OSM';
 
 window.OLMap = OLMap;
 window.OLView = OLView;
 window.OLTileLayer = OLTileLayer;
 window.OLOsm = OLOsm;
-window.OLProj = OLProj;
+window.OLProj = OLProj; */
+
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import * as Proj from 'ol/proj';
+import * as Layer from 'ol/layer';
+import * as Source from 'ol/source';
+import Feature from 'ol/Feature.js';
+import Overlay from 'ol/Overlay.js';
+import * as Geom from 'ol/geom';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+
+window.Map = Map;
+window.OSM = OSM;
+window.TileLayer = TileLayer;
+window.View = View;
+window.Feature = Feature;
+window.Layer = Layer;
+window.Source = Source;
+window.Proj = Proj;
+window.Geom = Geom;
+window.Style = Style;
+window.Icon = Icon;
+window.Overlay = Overlay;
 
 
--- resources/views/web/home/scripts.blade.php
+++ resources/views/web/home/scripts.blade.php
@@ -2,19 +2,81 @@
 
     function init() {
 
-        const center = new OLProj.fromLonLat([{{ $lon }}, {{ $lat }}]);
+        const center = new Proj.fromLonLat([{{ $lon }}, {{ $lat }}]);
 
-        const map = new OLMap({
+        const map = new Map({
             layers: [
-                new OLTileLayer({
-                source: new OLOsm(),
+                new TileLayer({
+                    source: new OSM(),
                 }),
             ],
             target: 'map',
-            view: new OLView({
+            view: new View({
                 center: center,
                 zoom: {{ OSM_ZOOM_DEFAULT }},
             }),
+        });
+
+        // Crea una instància de Feature amb la geometria del punt
+        var feature = new Feature({
+            geometry: new Geom.Point(Proj.fromLonLat([4.35247, 50.84673]))
+        });
+
+        // Defineix l'estil amb una icona personalitzada
+        var style = new Style({
+            image: new Icon({
+                src: "{{ asset('storage/img/icon.png') }}",
+                scale: 0.8, // Escala de l'icona (opcional)
+                // Altres opcions d'estil d'icona, com ara l'opacitat, l'angle, etc.
+            })
+        });
+
+        // Aplica l'estil a la característica
+        feature.setStyle(style);
+
+        // Crea una capa vectorial amb la font de vectors i la característica
+        var layer = new Layer.Vector({
+            source: new Source.Vector({
+                features: [feature]
+            })
+        });
+
+        map.addLayer(layer);
+
+        /**
+         * per obrir un pop-up al clicar a la posició
+         * però no és el que es vol...
+         */
+
+        var container = document.getElementById('popup');
+        var content = document.getElementById('popup-content');
+        var closer = document.getElementById('popup-closer');
+
+        var overlay = new Overlay({
+            element: container,
+            autoPan: true,
+            autoPanAnimation: {
+                duration: 250
+            }
+        });
+        map.addOverlay(overlay);
+
+        closer.onclick = function() {
+            overlay.setPosition(undefined);
+            closer.blur();
+            return false;
+        };
+
+        map.on('singleclick', function (event) {
+            if (map.hasFeatureAtPixel(event.pixel) === true) {
+                var coordinate = event.coordinate;
+
+                content.innerHTML = '<b>Hello world!</b><br />I am a popup.';
+                overlay.setPosition(coordinate);
+            } else {
+                overlay.setPosition(undefined);
+                closer.blur();
+            }
         });
     }
 
